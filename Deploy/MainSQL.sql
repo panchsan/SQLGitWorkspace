7f9062091677e706b61433ceca54d6a15c4ebfdb
panchsan <panchsan@outlook.com>
2020-09-09 22:43:20 -0400

    Database scripts added

---
+++ Deploy/MainSQL.sql
@@ -0,0 +1,8 @@
+CREATE TABLE city (
+    id int  NOT NULL IDENTITY(1, 1),
+    city_name char(128)  NOT NULL,
+    lat decimal(9,6)  NOT NULL,
+    long decimal(9,6)  NOT NULL,
+    country_id int  NOT NULL,
+    CONSTRAINT city_pk PRIMARY KEY  (id)
+);
