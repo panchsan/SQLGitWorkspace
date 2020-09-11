a5a22ed62d20dc65accdbde660f8aad9cf3a3d11
panchsan <panchsan@outlook.com>
2020-09-10 22:46:51 -0400

    Double quotes added in the Master.sql file

--- Deploy/MasterSQL.sql
+++ Deploy/MasterSQL.sql
@@ -1,9 +1,9 @@
 USE [JenkinsCICD]
 GO
 
-:r C:\Program Files\Jenkins\SQLGitWorkspace\Deploy\SQLQuery2.sql
+:r "C:\Program Files\Jenkins\SQLGitWorkspace\Deploy\SQLQuery2.sql"
 GO
-:r C:\Program Files\Jenkins\SQLGitWorkspace\Deploy\SQLQuery3.sql
+:r "C:\Program Files\Jenkins\SQLGitWorkspace\Deploy\SQLQuery3.sql"
 GO
-:r C:\Program Files\Jenkins\SQLGitWorkspace\Deploy\SQLQuery1.sql
+:r "C:\Program Files\Jenkins\SQLGitWorkspace\Deploy\SQLQuery1.sql"
 GO
