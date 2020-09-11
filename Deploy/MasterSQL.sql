d058382ce0b827f061b456d6b4ec5c9a5ac2bf95
panchsan <panchsan@outlook.com>
2020-09-10 22:40:05 -0400

    Added Master Script

---
+++ Deploy/MasterSQL.sql
@@ -0,0 +1,9 @@
+USE [JenkinsCICD]
+GO
+
+:r C:\Program Files\Jenkins\SQLGitWorkspace\Deploy\SQLQuery2.sql
+GO
+:r C:\Program Files\Jenkins\SQLGitWorkspace\Deploy\SQLQuery3.sql
+GO
+:r C:\Program Files\Jenkins\SQLGitWorkspace\Deploy\SQLQuery1.sql
+GO
